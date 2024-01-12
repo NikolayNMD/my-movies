5b2d7e12392832f7ff3daed155cec267af352d28
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2024-01-12 19:09:24 +0200

    added API, routes and LAZY

---
+++ src/services/api.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+const API_KEY = '2e6e31a453d6f91b41dab389038f4dbc';
+
+export const getTrending = async () => {
+  const response = await axios.get(`trending/movie/day?api_key=${API_KEY}`);
+  return response.data.results;
+};
+
+export const getSearchByKeyword = async keyword => {
+  const response = await axios.get(
+    `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${keyword}`
+  );
+  return response.data.results;
+};
+
+export const getMovieDetails = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data;
+};
+
+export const getActors = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data.cast;
+};
+
+export const getReviews = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return response.data.results;
+};
